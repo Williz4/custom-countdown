7f3d7b031f0365e28d0303f2553dcd8933242103
williz4 <wilsonmwangeka@gmail.com>
2022-02-22 07:41:04 -0500

    Added Event Listener to the form to pick the title and date values

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 //Set Date Input Minimum with Today's Date
 const today = new Date().toISOString().split('T')[0];
-console.log(today);
 
 dateEl.setAttribute('min', today);
+
+//Take Values from Form input
+function updateCountDown(e) {
+	e.preventDefault();
+
+	countdownTitle = e.srcElement[0].value;
+	countdownDate = e.srcElement[1].value;
+}
+
+//Event Listener
+countdownForm.addEventListener('submit', updateCountDown);
